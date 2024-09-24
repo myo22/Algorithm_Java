361190104ddc7efee579f12fa09b6bcd5ae064ff
myo22 <dlalsgud12@naver.com>
2024-09-24 15:59:10 +0900

    [11005] 진법 변환 2

--- src/BruteForce/Algorithm_11005.java
+++ src/BruteForce/Algorithm_11005.java
@@ -1,6 +1,8 @@
-import java.util.*;
+package BruteForce;
 
-class Main {
+import java.util.Scanner;
+
+public class Algorithm_11005 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
