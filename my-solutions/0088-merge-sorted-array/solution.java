d9c60a1d53036dcbe8514dd47836530125f61c5e
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-08-24 21:59:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.7 MB (78.69%)

--- my-solutions/0088-merge-sorted-array/solution.java
+++ my-solutions/0088-merge-sorted-array/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int length = m + n - 1;
+        int length = n + m - 1;
         for (int i = length; i >= 0; i--) {
-            if (m > 0 && (n == 0 || nums1[m - 1] >= nums2[n - 1])) {
+            if (m > 0 && (n <= 0 || nums1[m - 1] >= nums2[n - 1])) {
                 nums1[i] = nums1[m - 1];
                 m--;
             } else {
-                nums1[i] = nums2[n - 1];
+                nums1[i] = nums2[n -1];
                 n--;
-            }           
+            }
         }
     }
 }
