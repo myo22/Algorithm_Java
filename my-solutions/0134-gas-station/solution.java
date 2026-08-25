5c75c2869a569be5035d61ea0dd51184e89ae8b7
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-08-25 22:07:00 +0000

    Sync LeetCode submission Runtime - 2 ms (99.15%), Memory - 119.7 MB (10.31%)

--- my-solutions/0134-gas-station/solution.java
+++ my-solutions/0134-gas-station/solution.java
@@ -1,19 +1,19 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int n = gas.length;
-        int current = 0;
         int total = 0;
-        int index = 0;
+        int current = 0;
+        int start = 0;
         for (int i = 0; i < n; i++) {
-            int distance = gas[i] - cost[i];
-            total += distance;
-            current += distance;
+            int diss = gas[i] - cost[i];
+            current += diss;
+            total += diss;
+            
             if (current < 0) {
+                start = i + 1;
                 current = 0;
-                index = i + 1;                          
             }
         }
-        
-        return total >= 0 ? index : -1;
+        return total >= 0 ? start : -1;
     }
 }
