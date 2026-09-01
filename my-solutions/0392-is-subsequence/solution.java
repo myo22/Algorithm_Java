46be6ecb28e2a3ed23e8924f88dc2ce4bad18e28
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-09-01 22:13:00 +0000

    Sync LeetCode submission Runtime - 2 ms (73.41%), Memory - 43 MB (27.04%)

--- my-solutions/0392-is-subsequence/solution.java
+++ my-solutions/0392-is-subsequence/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if (s.length() == 0) return true;
+        if (s.isEmpty()) return true;
         
-        int sIdx = 0;
-        int tIdx = 0;
-        
-        while (sIdx < s.length() && tIdx < t.length()) {
-            if (s.charAt(sIdx) == t.charAt(tIdx)) {
-                sIdx++; // s의 다음 문자를 찾기 위해 포인터 이동
+        int sIndex = 0;
+        for (int tIndex = 0; tIndex < t.length(); tIndex++) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                sIndex++;
             }
-            tIdx++; // t는 무조건 다음 문자로 이동
+            if (sIndex == s.length()) {
+                return true;
+            }
         }
-        
-        // s의 모든 문자를 순서대로 다 찾았다면 sIdx가 s.length()와 같아짐
-        return sIdx == s.length();
+        return false;
     }
 }
 
