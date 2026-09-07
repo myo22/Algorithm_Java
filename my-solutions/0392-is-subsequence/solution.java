446176c3c05c4ffd0c3b47b48d2ccc17fc8b33a4
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-09-07 21:59:51 +0000

    Sync LeetCode submission Runtime - 2 ms (73.16%), Memory - 42.4 MB (95.61%)

--- my-solutions/0392-is-subsequence/solution.java
+++ my-solutions/0392-is-subsequence/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         if (s.length() == 0) return true;
-        int start = 0;
-        int end = t.length();
-        for (int i = 0; i < end; i++) {
-            if (s.charAt(start) == t.charAt(i)) {
-                start++;
+        int index = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(index) == t.charAt(i)) {
+                index++;
             }
-            if (start == s.length()) {
+            
+            if (index == s.length()) {
                 return true;
             }
         }
