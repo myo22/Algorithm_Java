7992f80be0478e3c8f5c5278441350dbfcf8e670
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-09-08 22:10:12 +0000

    Sync LeetCode submission Runtime - 5 ms (82.96%), Memory - 77.4 MB (49.05%)

--- my-solutions/0011-container-with-most-water/solution.java
+++ my-solutions/0011-container-with-most-water/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int maxArea(int[] height) {
-        int n = height.length;
         int left = 0;
-        int right = n - 1;
         int max = 0;
+        int right = height.length - 1;
         while (left <= right) {
             int area = (right - left) * Math.min(height[left], height[right]);
             max = Math.max(max, area);
