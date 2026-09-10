0e4bc9b3a1dad937706047ed4836ee926b116475
[통합인프라개발팀] 이민형 <rlaalsgud0125@gmail.com>
2026-09-10 22:20:05 +0000

    Sync LeetCode submission Runtime - 34 ms (58.05%), Memory - 59.3 MB (32.04%)

--- my-solutions/0015-3sum/solution.java
+++ my-solutions/0015-3sum/solution.java
@@ -1,27 +1,19 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list =  new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        
         for (int i = 0; i < nums.length - 2; i++) {
-            // 바깥쪽 중복 제거: 이전 숫자와 같으면 스킵
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            
             int left = i + 1;
             int right = nums.length - 1;
-            
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                
-                if (sum == 0) {                  
-                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
-                    // 안쪽 중복 제거 및 포인터 이동
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    
+                if (sum == 0) {
+                  list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                  while(left < right && nums[left] == nums[left + 1]) left++;
+                  while(left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
                 } else if (sum < 0) {
@@ -34,4 +26,3 @@ class Solution {
         return list;
     }
 }
-
